e71ac753b9aa9a5e90c88dc1aab60183a6fc88df
DESKTOP-J34JKBB\autho <authorsarp@hotmail.com>
2020-11-06 09:24:51 +0300

    ver 1.0.0

---
+++ MathGam/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MathGam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SUBMİT YOUR ANSWER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.clack);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Creating";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "True Answers: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "False Answers: ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(158, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Null Answers: ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 180);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Points: ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 225);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Math Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MathGam/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathGam
+{
+    public partial class Form1 : Form
+    {
+        private const string V = " + ";
+        int s1;
+        int s2;
+        int ss;
+        int yy;
+        int pp;
+        int dd;
+        int bb;
+        //newmode
+        int im;
+        //newmode
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            im = random.Next(4);
+            createAQ(im);
+        }
+
+        public void clack(object sender, EventArgs e)
+        {
+            String gir = textBox1.Text;
+            if(gir == Convert.ToString(ss))
+            {
+                pp += 5;
+                dd += 1;
+                im = random.Next(4);
+                createAQ(im);
+            }
+            else if(gir == "")
+            {
+                bb += 1;
+                im = random.Next(4);
+                createAQ(im);
+            }
+            else
+            {
+                yy += 1;
+                pp -= 2;
+                im = random.Next(4);
+                createAQ(im);
+            }
+        }
+        public void createAQ(int imm)
+        {
+            switch (imm)
+            {
+                case 0:
+                    s1 = random.Next(1,16);
+                    s2 = random.Next(1,16);
+                    ss = s1 + s2;
+                    Thread.Sleep(500);
+                    label1.Text = " " + s1 + " + " + s2 + " ";
+                    label2.Text = "True Answers: " + dd;
+                    label3.Text = "False Answers: " + yy;
+                    label4.Text = "Null Answers: " + bb;
+                    label5.Text = "Points: " + pp;
+                    break;
+                case 1:
+                    s1 = random.Next(1,16);
+                    s2 = random.Next(1,16);
+                    if (s2 > s1)
+                    {
+                        ss = s2 - s1;
+                        Thread.Sleep(500);
+                        label1.Text = " " + s2 + " - " + s1 + " ";
+                    }
+                    else
+                    {
+                        ss = s1 - s2;
+                        Thread.Sleep(500);
+                        label1.Text = " " + s1 + " - " + s2 + " ";
+                    }
+                    label2.Text = "True Answers: " + dd;
+                    label3.Text = "False Answers: " + yy;
+                    label4.Text = "Null Answers: " + bb;
+                    label5.Text = "Points: " + pp;
+                    break;
+                case 2:
+                    s1 = random.Next(1,16);
+                    s2 = random.Next(1,16);
+                    ss = s1 * s2;
+                    Thread.Sleep(500);
+                    label1.Text = " " + s1 + " x " + s2 + " ";
+                    label2.Text = "True Answers: " + dd;
+                    label3.Text = "False Answers: " + yy;
+                    label4.Text = "Null Answers: " + bb;
+                    label5.Text = "Points: " + pp;
+                    break;
+                case 3:
+                    do {
+                        s1 = random.Next(1, 16);
+                        s2 = random.Next(1, 16);
+                    } while (s1 % s2 != 0);
+                    if (s2 > s1)
+                    {
+                        ss = s2 / s1;
+                        Thread.Sleep(500);
+                        label1.Text = " " + s2 + " / " + s1 + " ";
+                    }
+                    else
+                    {
+                        ss = s1 / s2;
+                        Thread.Sleep(500);
+                        label1.Text = " " + s1 + " / " + s2 + " ";
+                    }
+                    label2.Text = "True Answers: " + dd;
+                    label3.Text = "False Answers: " + yy;
+                    label4.Text = "Null Answers: " + bb;
+                    label5.Text = "Points: " + pp;
+                    break;
+
+            }
+        }
+    }
+}
